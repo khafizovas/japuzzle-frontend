341206a9f8eef98a9d2628fcf5106cb3693d6142
khafizovas <justhomosapiens121@gmail.com>
2021-09-29 20:52:02 +0300

    Modify Field component to have an input table

--- src/Field/Field.js
+++ src/Field/Field.js
@@ -2,19 +2,30 @@ import './Field.css';
 
 const Field = (props) => {
 	return (
-		<div id='field'>
-			<table>
-				<tbody>
-					{[...Array(props.size.height)].map((_, i) => (
-						<tr key={i}>
-							{[...Array(props.size.width)].map((_, j) => (
-								<td key={i * props.size.width + j}></td>
-							))}
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+		<table id='game-field'>
+			<tbody>
+				<tr>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td></td>
+					<td>
+						<table id='game-input-field'>
+							<tbody>
+								{[...Array(props.size.height)].map((_, i) => (
+									<tr key={i}>
+										{[...Array(props.size.width)].map((_, j) => (
+											<td key={i * props.size.width + j} class='game-cell'></td>
+										))}
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</td>
+				</tr>
+			</tbody>
+		</table>
 	);
 };
 
