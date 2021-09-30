62085a70187eab397a318b35dc1f3799000865af
khafizovas <justhomosapiens121@gmail.com>
2021-10-01 01:16:27 +0300

    Add selectedColor state to App component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 		hints: {
 			rows: [
 				[
-					{ color: '#ffffff', count: 1 },
+					{ color: '#000000', count: 1 },
 					{ color: '#000000', count: 1 },
 				],
 				[{ color: '#000000', count: 5 }],
@@ -50,7 +50,20 @@ const App = () => {
 		Array(taskSize.height).fill(Array(taskSize.width).fill(0))
 	);
 
-	return <Field size={taskSize} hints={task.hints} />;
+	// Hardcoded
+	// TODO Palette component and using of state setter in it
+	const [selectedColor, setSelectedColor] = useState('#000000');
+
+	const changeSolution = (coordinate) => {
+		const newSolution = JSON.parse(JSON.stringify(solution));
+		newSolution[coordinate.x][coordinate.y] = selectedColor;
+
+		setSolution(newSolution);
+	};
+
+	return (
+		<Field size={taskSize} hints={task.hints} changeSolution={changeSolution} />
+	);
 };
 
 export default App;
