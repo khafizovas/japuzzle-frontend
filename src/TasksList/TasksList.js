a272775675d76a6b712b83fbe0f0ec7a289bc120
khafizovas <justhomosapiens121@gmail.com>
2021-10-25 20:24:39 +0300

    Added TasksList styles

--- src/TasksList/TasksList.js
+++ src/TasksList/TasksList.js
@@ -1,8 +1,10 @@
 import Task from '../Task/Task';
 
+import './TasksList.css';
+
 const TasksList = (props) => {
 	return (
-		<div>
+		<div id='tasks-list'>
 			{props.tasks.map((task, i) => (
 				<Task key={i} info={task} selectTask={props.selectTask} />
 			))}
