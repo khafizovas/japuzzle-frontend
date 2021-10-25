23fed3bf853448ff596df83873e1ca3c3d275857
khafizovas <justhomosapiens121@gmail.com>
2021-10-25 19:57:26 +0300

    Added authorization

--- src/Authorization/Authorization.js
+++ src/Authorization/Authorization.js
@@ -1,11 +1,48 @@
+import { useState } from 'react';
+
 const Authorization = (props) => {
+	// TODO: remove pojo
+	const [user, setUser] = useState('pojo');
+	const [message, setMessage] = useState('Sample');
+
+	const signIn = (ev) => {
+		ev.preventDefault();
+
+		const login = document.querySelector('#user').value;
+
+		fetch(
+			`https://japuzzle-backend.herokuapp.com/api/user/register/?user=${user}`
+		)
+			.then((response) => {
+				console.log(response.status);
+				return [response.status, response.json()];
+			})
+			.then(([status, data]) => {
+				if (status === 302 || status === 201) {
+					setUser(login);
+					setMessage(data);
+				}
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+
 	return (
 		<div>
-			<p>
-				It'll be proper authorization and registration there, but now just click
-				the button below (:
-			</p>
-			<button onClick={() => props.signIn('pojo')}>Sign in</button>
+			{message.length ? (
+				<>
+					{message}
+					<br />
+					<button onClick={() => props.signIn(user)}>Continue</button>
+				</>
+			) : (
+				<form onSubmit={signIn}>
+					<label htmlFor='user'>Your username: </label>
+					<input id='user' type='text' />
+					<button type='submit'>Enter</button>
+				</form>
+			)}
 		</div>
 	);
 };
