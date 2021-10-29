a62b799af903016515239585ff033ba180f8be9f
khafizovas <justhomosapiens121@gmail.com>
2021-10-29 04:44:44 +0300

    Edited goBack button in TasksLists

--- src/components/TasksList/TasksList.js
+++ src/components/TasksList/TasksList.js
@@ -4,12 +4,16 @@ import '../../styles/TasksList.css';
 
 const TasksList = (props) => {
 	return (
-		<div id='tasks-list'>
-			{props.tasks.map((task, i) => (
-				<Task key={i} info={task} selectTask={props.selectTask} />
-			))}
-			<button onClick={props.goBack}>Go back</button>
-		</div>
+		<>
+			<button id='tasks-goback' onClick={props.goBack}>
+				Go back
+			</button>
+			<div id='tasks-list'>
+				{props.tasks.map((task, i) => (
+					<Task key={i} info={task} selectTask={props.selectTask} />
+				))}
+			</div>
+		</>
 	);
 };
 
